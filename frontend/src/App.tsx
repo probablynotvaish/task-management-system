33fc16beb5a302c7453362d05651a531865fdbaf
Vaishnavi Gaddalay <vaishgadd8@gmail.com>
2026-04-07 23:31:34 +0530

    frontend initialization

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,36 @@
+import "./login.css";
+
+function Login() {
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        
+        <div className="icon-circle">✓</div>
+
+        <h2 className="title">Welcome Back</h2>
+        <p className="subtitle">
+          Sign in to your account to manage your tasks
+        </p>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input type="email" placeholder="Enter your email" />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input type="password" placeholder="Enter your password" />
+        </div>
+
+        <button className="login-btn">Sign In</button>
+
+        <p className="signup-text">
+          Don’t have an account? <span>Sign up</span>
+        </p>
+
+      </div>
+    </div>
+  );
+}
+
+export default Login;
